bfee279b753fa71e917994af8da6e50cab0b1e0c
Adarsha-React <adarshareact2023@gmail.com>
2023-08-06 09:41:34 +0530

    Binary Search update

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const nums = [1, 4, 5, 8, 3, 9];
-const target = 9;
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+const target = 16;
 
-const linearSearch = (nums, target) => {
-  let steps = 0;
-  for (let i = 0; i < nums.length; i++) {
+const binarySearch = (nums, target) => {
+  let steps = 0,
+    start = 0,
+    end = nums.length - 1;
+
+  while (start <= end) {
     steps++;
-    if (nums[i] === target) {
-      console.log("Number of steps taken by Linear Search O(n): " + steps);
-      return i;
+    let mid = Math.round((start + end) / 2);
+    if (nums[mid] === target) {
+      console.log("Number of steps taken by Binary Search O(log n) : " + steps);
+      return mid;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
     }
   }
   return -1;
 };
 
-let result = linearSearch(nums, target);
+let result = binarySearch(nums, target);
 if (result === -1) {
   console.log("Target not found");
 } else {
