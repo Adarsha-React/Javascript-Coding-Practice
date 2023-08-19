ee7c1b4a00948ce3f697567f4dc763c7fd983227
Adarsha-React <adarshareact2023@gmail.com>
2023-08-19 19:58:06 +0530

    Local storage functions

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 console.log("Start");
 
-try {
-  console.log("Inside try block - first line");
+const user = {
+  name: "Adarsha",
+  age: 31,
+};
 
-  someFunction();
+const jsonData = JSON.stringify(user);
+console.log(jsonData);
 
-  console.log("Inside try block - last line");
-} catch (error) {
-  console.log(error.message);
-} finally {
-  console.log("Finally block, runs whatever it is");
+localStorage.setItem("user1", jsonData);
+localStorage.setItem("user2", jsonData);
+
+localStorage.setItem("user3", jsonData);
+localStorage.setItem("user4", jsonData);
+
+const userFromLS = JSON.parse(localStorage.getItem("user1"));
+console.log(userFromLS);
+
+console.log(localStorage);
+
+for (let i = 0; i < localStorage.length; i++) {
+  console.log(localStorage.key(i));
 }
-
-console.log("End");
