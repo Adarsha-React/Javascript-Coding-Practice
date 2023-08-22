c0dce0e72888a3e1abc40b274dd51705b77ab158
Adarsha-React <adarshareact2023@gmail.com>
2023-08-22 20:58:06 +0530

    Deboucing

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-//Two point sum = 0
-const numbers = [-6, -3, 1, 2, 3, 4, 5, 7];
-
-const twoPointSum = (numbers) => {
-  //Initialize two pointers
-  let start = 0,
-    end = numbers.length - 1,
-    sum = 0;
-
-  while (start < end) {
-    sum = numbers[start] + numbers[end];
-    if (sum === 0) {
-      console.log(`Indices are - ${start} and ${end}`);
-      break;
-    } else if (sum < 0) {
-      start++;
-    } else {
-      end--;
-    }
-  }
-  return null; // if no pair found
+const getData = () => {
+  console.log("Fetching the data");
 };
 
-twoPointSum(numbers); //output - Indices are - 1 and 4
+const debounce = (getData, delay) => {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      getData();
+    }, delay);
+  };
+};
+
+const betterFunction = debounce(getData, 1000);
