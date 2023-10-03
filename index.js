2040e401f03b4a6b293d1183d9f6e4c3acec4393
= <=>
2023-10-03 15:28:42 +0530

    Polyfill for filter

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const nums = [3, 5, 7, 9, 11, 13];
 
-const result = nums.map((item) => item * 2);
+const result = nums.filter((item) => item > 9);
 console.log(result);
 
-Array.prototype.myMap = function (callBack) {
-  let context = this;
-  let result = [];
-  console.log(callBack);
+Array.prototype.myFilter = function (callBack) {
+  let context = this,
+    result = [];
   for (let i = 0; i < context.length; i++) {
-    result.push(callBack(context[i]));
+    if (callBack(context[i])) {
+      result.push(context[i]);
+    }
   }
   return result;
 };
 
-const newResult = nums.myMap(function (item) {
-  return item * 3;
-});
+const newResult = nums.myFilter((item) => item > 9);
 console.log(newResult);
