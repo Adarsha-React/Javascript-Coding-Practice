0f66f009392bf7302283cec439eee8076a3287b5
= <=>
2023-10-03 21:19:04 +0530

    Binary Search

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const nums = [4, 5, 6, 2, 10, 66];
+const nums = [4, 6, 10, 20, 50, 70];
 console.log(nums);
 
-function linearSearch(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
-      return true;
+function binarySearch(nums, target) {
+  let left = 0,
+    right = nums.length,
+    mid = 0;
+
+  console.log(right);
+
+  while (left <= right) {
+    mid = (left + right) / 2;
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
+
   return -1;
 }
 
-const result = linearSearch(nums, 20);
+const result = binarySearch(nums, 50);
 console.log(result);
