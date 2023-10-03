c97aa72eea6035bf3d28e385eab2ce8d535c52bc
= <=>
2023-10-03 14:57:05 +0530

    Javascript Deboucing

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const nums = [6, 5, 2, 8, 9, 4];
-// let minIndex = -1;
-let temp = 0;
+let count = 1;
+function getData() {
+  console.log("Fecthing the data...", count++);
+}
 
-const selectionSort = (nums) => {
-  let size = nums.length;
-  let minIndex = -1;
-  for (let i = 0; i < size; i++) {
-    minIndex = i;
-    console.log("Outer Loop.................", i);
-    for (let j = i + 1; j < size; j++) {
-      console.log("Inner Loop....", j);
-      if (nums[j] < nums[minIndex]) {
-        minIndex = j;
-      }
-    }
+function debounce(getData, delay) {
+  let context = this,
+    args = arguments,
+    timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      getData.call(context, args);
+    }, delay);
+  };
+}
 
-    if (minIndex != i) {
-      temp = nums[i];
-      nums[i] = nums[minIndex];
-      nums[minIndex] = temp;
-
-      console.log(nums);
-    }
-  }
-  return nums;
-};
-
-console.log("Before Sort: " + nums);
-const result = selectionSort(nums);
-console.log("After sort: " + result);
+const betterFunction = debounce(getData, 500);
